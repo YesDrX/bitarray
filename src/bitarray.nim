67fe2a410fdd6cab764a75884374ce49cb6b63cd
Yes DrX <yes.drx@gmail.com>
2020-08-09 16:50:44 -0400

    add reverseBits

--- src/bitarray.nim
+++ src/bitarray.nim
@@ -18,7 +18,8 @@ export bitops, strutils, utils
 ## 
 ## You may change the underlying BlockInt definition (in utils) to force define BlockInt to a different len, say 16-bits on a 64-bits CPU.
 ## 
-## NOTE: Bits are left aligned, so least significant bit is at location 0. So 10000110 is equal to 127 ('a'), rather than 134.
+## NOTE: Bits are left aligned, so least significant bit is at location 0. So 10000110 is equal to 127 ('a'), rather than 134. Use proc reverseBits when necessary.
+## 
 ## 
 type
   BitsArray* = ref object
